d46df37abfc6370ba630915b99385463df17afdc
Marianne Bravo <mloubravo@outlook.com>
2023-07-29 19:21:48 -0300

    [✅] Make test pass

--- 2-quarter-of-the-year/src/NOTES.md
+++ 2-quarter-of-the-year/src/NOTES.md
@@ -43,3 +43,4 @@ Constraint:
 [✅] Make test pass
 
 [✅] Create test: should return "1st Quarter" when the month 1 is given
+[✅] Make test pass
--- 2-quarter-of-the-year/src/index.js
+++ 2-quarter-of-the-year/src/index.js
@@ -2,4 +2,8 @@ function verify_month(num) {
   return (num >= 1 && num <= 12) ? true : false;
 }
 
-module.exports = { verify_month }
+function quarter_of_the_year(num) {
+  return "1st Quarter";
+}
+
+module.exports = { verify_month, quarter_of_the_year }
