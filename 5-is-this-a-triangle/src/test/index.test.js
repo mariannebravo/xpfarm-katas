dbd47bbeb19fb6272fc86a322973e6e19ccab140
Marianne Bravo <mloubravo@outlook.com>
2023-07-30 19:22:36 -0300

    [✅] Create test: return 'Zero is not valid' when the second value is zero

--- 5-is-this-a-triangle/src/test/index.test.js
+++ 5-is-this-a-triangle/src/test/index.test.js
@@ -5,4 +5,8 @@ describe('Is this a triangle', () => {
   it('should verify if any of the numbers given are different than zero', () => {
     expect(is_this_a_triangle(0, 0, 0)).toStrictEqual('Zero is not valid.');
   });
+
+  it("return 'Zero is not valid' when the second value is zero", () => {
+    expect(is_this_a_triangle(1, 0, 1)).toStrictEqual('Zero is not valid.');
+  })
 });
